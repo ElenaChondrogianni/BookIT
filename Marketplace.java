66b1764ce8377a9112c3fde6cd0169adebbac078
ElenaChondrogianni <elena.funny29@gmail.com>
2023-05-28 20:49:29 +0300

    marketplace

---
+++ Marketplace.java
@@ -0,0 +1,9 @@
+package BookIT;
+import java.util.*;
+
+public class Marketplace {
+    public static List<Book> books = new ArrayList<Book>();
+    public static List<User> users = new ArrayList<User>();
+
+    
+}
