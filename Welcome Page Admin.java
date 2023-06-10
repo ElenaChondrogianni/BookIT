c6a74a7e2e753edbd43c5e82188b54c907de25b0
Giorgos Souvaliotis <giorgosouvis@gmail.com>
2023-06-10 23:05:04 +0300

    Update Welcome Page Admin.java
    Working imports

--- Welcome Page Admin.java
+++ Welcome Page Admin.java
@@ -1,7 +1,7 @@
-//import javax.faces.application.FacesMessage;
-//import javax.faces.bean.ManagedBean;
-//import javax.faces.bean.RequestScoped;
-//import javax.faces.context.FacesContext;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 @RequestScoped
